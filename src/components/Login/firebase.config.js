df2a8c0d481423585d8ffb9d63a365613bc5b341
Anik <aniksarker1612@gmail.com>
2020-09-29 00:56:54 +0800

    burj al arab hotel booking site resources added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSQMbztgj7Qh6TF3YhaFspVwyLY9PVsVY",
+  authDomain: "burj-al-arab-62315.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-62315.firebaseio.com",
+  projectId: "burj-al-arab-62315",
+  storageBucket: "burj-al-arab-62315.appspot.com",
+  messagingSenderId: "590419212902",
+  appId: "1:590419212902:web:9edd8397ca810acc880ba7"
+  };
+  export default firebaseConfig;
